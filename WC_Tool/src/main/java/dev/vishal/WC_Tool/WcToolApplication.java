f647ecc3de44758e966dec4a23bec1402dc04731
klahsiv <vishalkumar2803001@gmail.com>
2024-11-02 09:12:10 +0530

    Initial Commit

---
+++ WC_Tool/src/main/java/dev/vishal/WC_Tool/WcToolApplication.java
@@ -0,0 +1,13 @@
+package dev.vishal.WC_Tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WcToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WcToolApplication.class, args);
+	}
+
+}
---
+++ WC_Tool/src/test/java/dev/vishal/WC_Tool/WcToolApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vishal.WC_Tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WcToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
