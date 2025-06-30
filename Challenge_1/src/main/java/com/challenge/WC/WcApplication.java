7fc272a2e21b3191e63e37dae5054080a61ddb12
klahsiv <vishalkumar2803001@gmail.com>
2025-06-30 20:23:07 +0530

    Setup For Challenge 1

---
+++ Challenge_1/src/main/java/com/challenge/WC/WcApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.WC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WcApplication.class, args);
+	}
+
+}
---
+++ Challenge_1/src/test/java/com/challenge/WC/WcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.WC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
